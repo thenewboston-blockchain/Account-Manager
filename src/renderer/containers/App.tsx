6f376ef6e81ce924c74ce83abb5ee4949a20b07d
Justin Kim <justinkim943@gmail.com>
2020-05-25 19:51:07 -0700

    absolute paths > relative paths

--- src/renderer/containers/App.tsx
+++ src/renderer/containers/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-import Sample from "./Sample";
+import Sample from "@renderer/containers/Sample";
 
 export default hot(
   (): JSX.Element => (
--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './containers/App';
+import App from '@renderer/containers/App';
 import './renderer.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
