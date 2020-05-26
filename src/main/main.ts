b10dce13c927b0b145f86e37c1201e8146a92697
Justin Kim <justinkim943@gmail.com>
2020-05-25 18:58:52 -0700

    moved things around

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 const electronSquirrelStartup = require('electron-squirrel-startup');
 
-declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (electronSquirrelStartup) {
@@ -17,9 +17,6 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import App from './containers/App';
 import './renderer.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
