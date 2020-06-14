01fdc5d4a5cfa222a2780f34fda9e22d1d6cba82
Bucky <buckyroberts@gmail.com>
2020-06-13 17:45:35 -0700

    Work

--- src/renderer/containers/LeftSubmenu/index.tsx
+++ src/renderer/containers/LeftSubmenu/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 
 import './LeftSubmenu.scss';
 
@@ -7,11 +7,29 @@ interface ComponentProps {
 }
 
 const LeftSubmenu: FC<ComponentProps> = ({title}) => {
+  const [open, setExpanded] = useState<boolean>(true);
+
+  const toggleExpanded = (): void => {
+    setExpanded(!open);
+  };
+
   return (
     <div className="LeftSubmenu">
       <div className="LeftSubmenu__header">
-        <span className="header__title">{title}</span>
+        <div className="left-items">
+          <span className={`material-icons ${open ? 'open' : 'closed'}`} onClick={toggleExpanded}>
+            play_arrow
+          </span>
+          <span className="submenu-title">{title}</span>
+        </div>
+        <div className="right-items">
+          <span className="material-icons">add</span>
+        </div>
       </div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
     </div>
   );
 };
