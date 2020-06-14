7fcfe09f11242066b8e588ec155488bc6435bdcc
Bucky <buckyroberts@gmail.com>
2020-06-13 18:50:57 -0700

    Top nav

--- src/renderer/containers/TopNav/index.tsx
+++ src/renderer/containers/TopNav/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import {NavLink} from 'react-router-dom';
-
 import './TopNav.scss';
 
 const TopNav = () => {
   return (
     <div className="TopNav">
-      <NavLink className="thenewboston" to="/">
+      <div className="thenewboston">
         <img alt="thenewboston logo" className="logo" src={require('@renderer/assets/logo.png')} />
         <span>thenewboston</span>
-      </NavLink>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
+      </div>
     </div>
   );
 };
