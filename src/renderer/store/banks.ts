f58537d8c6dc20a2be4fcbb563330822491db287
Bucky <buckyroberts@gmail.com>
2020-06-14 15:46:36 -0700

    Check

--- src/renderer/store/banks.ts
+++ src/renderer/store/banks.ts
@@ -1,8 +1,15 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 interface Bank {
-  name: string;
-  ipAddress: string;
+  account_number: string;
+  ip_address: string;
+  network_identifier: string;
+  port?: number | null;
+  protocol: 'http' | 'https';
+  version: string;
+  default_transaction_fee: string;
+  registration_fee: string;
+  trust: string;
 }
 
 const banks = createSlice({
@@ -12,8 +19,17 @@ const banks = createSlice({
 });
 
 export const sampleBanks: Bank[] = [
-  {name: 'Bank 1', ipAddress: '169.189.222.35'},
-  {name: 'Bank 2', ipAddress: '242.96.35.2'},
+  {
+    account_number: '5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8',
+    ip_address: '192.168.1.232',
+    network_identifier: 'd5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1',
+    port: 8000,
+    protocol: 'http',
+    version: 'v1.0',
+    default_transaction_fee: '1.0000000000000000',
+    registration_fee: '2.0000000000000000',
+    trust: '100.00',
+  },
 ];
 
 export default banks;
