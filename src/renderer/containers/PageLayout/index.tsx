05267c58cb7542ea1ebd4d37b4bb64d8ca15e926
Bucky Roberts <buckyroberts@gmail.com>
2020-06-15 17:13:01 -0700

    Move pagination (#5)

--- src/renderer/containers/PageLayout/index.tsx
+++ src/renderer/containers/PageLayout/index.tsx
@@ -3,17 +3,15 @@ import React, {FC, ReactNode} from 'react';
 import './PageLayout.scss';
 
 interface ComponentProps {
+  content: ReactNode;
   top: ReactNode;
-  middle: ReactNode;
-  bottom?: ReactNode;
 }
 
-const PageLayout: FC<ComponentProps> = ({bottom, middle, top}) => {
+const PageLayout: FC<ComponentProps> = ({content, top}) => {
   return (
     <div className="PageLayout">
       <div className="PageLayout-header">{top}</div>
-      <div className="PageLayout-content">{middle}</div>
-      {bottom && <div className="PageLayout-footer">{bottom}</div>}
+      <div className="PageLayout-content">{content}</div>
     </div>
   );
 };
