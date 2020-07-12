bab63642cef186fb9c9e313895d43a669c04083c
Justin Kim <justinkim943@gmail.com>
2020-07-12 13:20:19 -0700

    updated width of formInputs, and also finished the basics of AddAccountModal

--- src/renderer/containers/Account/AddAccountModal/index.tsx
+++ src/renderer/containers/Account/AddAccountModal/index.tsx
@@ -1,15 +1,44 @@
 import React, {FC} from 'react';
-import noop from 'lodash/noop';
+import * as Yup from 'yup';
+
 import Modal from '@renderer/components/Modal';
+import useBooleanState from '@renderer/hooks/useBooleanState';
+import {FormInput} from '@renderer/components/FormComponents';
+
+const initialValues = {
+  nickname: '',
+};
+
+type FormValues = typeof initialValues;
+
+// TODO: Make it unique if inputted
+const validationSchema = Yup.object().shape({
+  nickname: Yup.string(),
+});
 
 interface ComponentProps {
   close(): void;
 }
 
 const AddAccountModal: FC<ComponentProps> = ({close}) => {
+  const [submitting, , setSubmittingTrue, setSubmittingFalse] = useBooleanState(false);
+
+  const handleSubmit = async (values: FormValues): Promise<void> => {
+    console.log('VALUES', values);
+  };
+
   return (
-    <Modal close={close} onSubmit={noop}>
-      Modal
+    <Modal
+      close={close}
+      header="Create Account"
+      ignoreDirty
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      submitButton="Create"
+      submitting={submitting}
+      validationSchema={validationSchema}
+    >
+      <FormInput label="Account Nickname" name="nickname" />
     </Modal>
   );
 };
