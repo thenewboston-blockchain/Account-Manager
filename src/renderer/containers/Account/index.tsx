48d97b61de551f0da9ad404f4f64eb881ce24263
sidou01 <sidou010@outlook.com>
2020-07-14 15:13:12 +0100

    account page ui updates

--- src/renderer/containers/Account/index.tsx
+++ src/renderer/containers/Account/index.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import noop from 'lodash/noop';
 
 import {Button} from '@renderer/components/FormElements';
 import DetailPanel from '@renderer/containers/DetailPanel';
+import DropdownMenuButton, {DropdownMenuOption} from '@renderer/components/DropdownMenuButton';
 import PageHeader from '@renderer/components/PageHeader';
 import PageLayout from '@renderer/containers/PageLayout';
 import PageTabs from '@renderer/components/PageTabs';
 import QR from '@renderer/components/QR';
+import Icon from '@renderer/components/Icon';
+import Modal from '@renderer/components/Modal';
+
+import useBooleanState from '@renderer/hooks/useBooleanState';
 
 import './Account.scss';
 
 const Account = () => {
+  const [deleteModalIsOpen, toggleDeleteModal] = useBooleanState(false);
+  const [submittingDeleteModal, , setSubmittingDeleteModalTrue, setSubmittingDeleteModalFalse] = useBooleanState(false);
+
+  const dropdownMenuOptions: DropdownMenuOption[] = [
+    {
+      label: 'Edit',
+      onClick: noop,
+    },
+    {
+      label: 'Delete Account',
+      onClick: toggleDeleteModal,
+    },
+  ];
+
+  const handleDeleteAccountFromModal = async (): Promise<void> => {
+    try {
+      setSubmittingDeleteModalTrue();
+      setTimeout(() => {
+        setSubmittingDeleteModalFalse();
+        toggleDeleteModal();
+      }, 1000);
+    } catch (error) {
+      console.log('ERROR', error);
+    }
+  };
+
   const renderDetailPanels = () => {
     return (
       <div className="detail-panels">
@@ -55,6 +87,32 @@ const Account = () => {
     );
   };
 
+  const renderDeleteModal = () => (
+    <Modal
+      cancelButton="Cancel"
+      className="Account__DeleteModal"
+      close={toggleDeleteModal}
+      header={
+        <>
+          <Icon className="Icon__warning" icon="warning" />
+          <h2 className="Modal__title">Delete Account</h2>
+        </>
+      }
+      onSubmit={handleDeleteAccountFromModal}
+      submitButton="Yes"
+      submitting={submittingDeleteModal}
+    >
+      <>
+        <span className="delete-warning-span">Warning: </span> If you delete your account, you will lose all the points
+        in your account as well as your signing key. Are you sure you want to delete your account?
+      </>
+    </Modal>
+  );
+
+  const renderLeftTools = () => {
+    return <DropdownMenuButton options={dropdownMenuOptions} />;
+  };
+
   const renderRightPageHeaderButtons = () => (
     <>
       <Button>Send Points</Button>
@@ -63,7 +121,11 @@ const Account = () => {
 
   const renderTop = () => (
     <>
-      <PageHeader rightContent={renderRightPageHeaderButtons()} title="Donations (43hawrjkef243d)" />
+      <PageHeader
+        leftTools={renderLeftTools()}
+        rightContent={renderRightPageHeaderButtons()}
+        title="Donations (43hawrjkef243d)"
+      />
       <PageTabs
         items={[
           {
@@ -82,6 +144,7 @@ const Account = () => {
   return (
     <div className="Account">
       <PageLayout content={renderDetailPanels()} top={renderTop()} />
+      {deleteModalIsOpen && renderDeleteModal()}
     </div>
   );
 };
