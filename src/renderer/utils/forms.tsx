471febab648e4f024085f5a80bb8ecc18f2c0b8e
Justin Kim <justinkim943@gmail.com>
2020-07-19 19:58:40 -0700

    cleaned up our form components

--- src/renderer/utils/forms.tsx
+++ src/renderer/utils/forms.tsx
@@ -3,14 +3,14 @@ import {ErrorMessage} from 'formik';
 import RequiredAsterisk from '@renderer/components/RequiredAsterisk';
 
 export const renderFormError = (name: string): ReactNode => (
-  <span className="error">
+  <span className={`FormFieldComponent__error-message`}>
     <ErrorMessage name={name} />
   </span>
 );
 
 export const renderFormLabel = (name: string, label?: string, required?: boolean): ReactNode =>
   label ? (
-    <label htmlFor={name}>
+    <label className={`FormFieldComponent__label`} htmlFor={name}>
       {label}
       {required ? <RequiredAsterisk /> : null}
     </label>
