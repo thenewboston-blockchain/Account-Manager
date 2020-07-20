ec6fef6df86c384b5a5cf2b209c007bf6c3a0714
Justin Kim <justinkim943@gmail.com>
2020-07-19 18:14:25 -0700

    cleanup

--- src/renderer/containers/TopNav/index.tsx
+++ src/renderer/containers/TopNav/index.tsx
@@ -7,16 +7,14 @@ import './TopNav.scss';
 const TopNav = () => {
   return (
     <div className="TopNav">
-      <div className="navigation-icons">
-        <div>
-          <Icon icon={IconType.arrowLeft} />
-          <Icon icon={IconType.arrowRight} />
-        </div>
-        <div>
-          <Icon icon={IconType.bell} />
-          <Icon icon={IconType.helpCircle} />
-          <Icon icon={IconType.cog} />
-        </div>
+      <div className="TopNav__container">
+        <Icon icon={IconType.arrowLeft} />
+        <Icon icon={IconType.arrowRight} />
+      </div>
+      <div className="TopNav__container">
+        <Icon icon={IconType.bell} />
+        <Icon icon={IconType.helpCircle} />
+        <Icon icon={IconType.cog} />
       </div>
     </div>
   );
