4929cd4e616b57c16d9f06e94a6680dbc80ef1a6
kanishk-anand-oyo <kanishk.anand@oyorooms.com>
2020-07-20 15:29:30 +0530

    Unit testing of format function in utils

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  modulePaths: ['node_modules', '<rootDir>/src', '<rootDir>'],
+  // An array of directory names to be searched recursively up from the requiring module's location
+  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'scss'],
+
+  // A map from regular expressions to module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
