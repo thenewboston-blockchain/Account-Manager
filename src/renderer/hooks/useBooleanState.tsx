086ad23d176582fdde9ab1b6f78d888b5d48a07f
Justin Kim <justinkim943@gmail.com>
2020-07-25 14:46:05 -0700

    make linting command work

--- src/renderer/hooks/useBooleanState.tsx
+++ src/renderer/hooks/useBooleanState.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 const useBooleanState = (initialValue: boolean): [boolean, () => void, () => void, () => void] => {
   const [state, setState] = useState(initialValue);
