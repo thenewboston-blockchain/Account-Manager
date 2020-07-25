e9612934be26df71105e53505002e9b40bb35174
Leela <leela.tanniru@gmail.com>
2020-07-25 23:56:46 +0530

    Assign nickname to managed banks

--- src/renderer/store/banks.ts
+++ src/renderer/store/banks.ts
@@ -1,8 +1,7 @@
-import {createSlice} from '@reduxjs/toolkit';
-
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {fetchBanks} from '@renderer/api/bank';
 import {BANKS} from '@renderer/constants/store';
-import {Bank} from '@renderer/types/entities/Bank';
+import {Bank, ProtocolType} from '@renderer/types/entities/Bank';
 import {Loading, StateSlice} from '@renderer/types/store';
 import {fulfilledReducer, pendingReducer, rejectedReducer} from '@renderer/utils/store';
 
@@ -19,14 +18,48 @@ const banksSlice = createSlice({
     loading: Loading.idle,
   } as StateSlice<Bank[]>,
   name: BANKS,
-  reducers: {},
+  reducers: {
+    updateNickname: {
+      prepare: (nickname = '') => {
+        return {
+          payload: {
+            account_number: '124_ACC',
+            default_transaction_fee: '1234',
+            ip_address: '198.168.1.1',
+            nickname: nickname || '',
+            node_identifier: '1234',
+            port: 1234,
+            protocol: 'http' as ProtocolType,
+            trust: '1234',
+            version: '1.2.3',
+          },
+        };
+      },
+      reducer: (state, action: PayloadAction<Bank>) => {
+        state.entities.push(action.payload);
+      },
+    },
+  },
 });
 
 export const sampleBanks: StateSlice<Bank[]> = {
   currentRequestId: undefined,
-  entities: [],
+  entities: [
+    {
+      account_number: '124',
+      default_transaction_fee: '1234',
+      ip_address: '198.168.1.0',
+      nickname: '',
+      node_identifier: '1234',
+      port: 123,
+      protocol: 'http',
+      trust: '1234',
+      version: '1.2.3',
+    },
+  ],
   error: null,
   loading: Loading.idle,
 };
+export const {updateNickname: updateBankNickname} = banksSlice.actions;
 
 export default banksSlice;
--- src/renderer/types/entities/Bank.d.ts
+++ src/renderer/types/entities/Bank.d.ts
@@ -1,10 +1,12 @@
+export type ProtocolType = 'http' | 'https';
 export interface Bank {
   account_number: string;
   ip_address: string;
   node_identifier: string;
   port: number | null;
-  protocol: 'http' | 'https';
+  protocol: ProtocolType;
   version: string;
   default_transaction_fee: string;
   trust: string;
+  nickname?: string;
 }
