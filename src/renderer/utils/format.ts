93812b15efc3e875b0727d69003208a75ed0370b
Justin Kim <justinkim943@gmail.com>
2020-07-24 19:59:59 -0700

    utils

--- src/renderer/utils/format.ts
+++ src/renderer/utils/format.ts
@@ -1,4 +1,4 @@
-export const formatAddress = (ipAddress: string, port: string|null, protocol:string) => {
-  const _port = (port && port !== '80') ? `:${port}` : '';
-  return `${protocol}://${ipAddress}${_port}`;
-}
+export const formatAddress = (ipAddress: string, port: string | null, protocol: string): string => {
+  const formattedPort = port && port !== '80' ? `:${port}` : '';
+  return `${protocol}://${ipAddress}${formattedPort}`;
+};
--- src/renderer/utils/store.ts
+++ src/renderer/utils/store.ts
@@ -1,15 +1,15 @@
 import {PayloadAction, SerializedError} from '@reduxjs/toolkit';
 import {Loading, StateSlice} from '@renderer/types/store';
 
-export const createActionType = (sliceName: string) => `${sliceName}/create`;
+export const createActionType = (sliceName: string): string => `${sliceName}/create`;
 
-export const deleteActionType = (sliceName: string) => `${sliceName}/delete`;
+export const deleteActionType = (sliceName: string): string => `${sliceName}/delete`;
 
-export const fetchActionType = (sliceName: string) => `${sliceName}/fetch`;
+export const fetchActionType = (sliceName: string): string => `${sliceName}/fetch`;
 
-export const fetchByIdActionType = (sliceName: string) => `${sliceName}/fetchById`;
+export const fetchByIdActionType = (sliceName: string): string => `${sliceName}/fetchById`;
 
-export const updateActionType = (sliceName: string) => `${sliceName}/update`;
+export const updateActionType = (sliceName: string): string => `${sliceName}/update`;
 
 type ActionType = PayloadAction<any, string, {arg: void; requestId: string}, never>;
 type RejectedActionType = PayloadAction<
@@ -20,14 +20,14 @@ type RejectedActionType = PayloadAction<
 >;
 type StateType = StateSlice<any>;
 
-export const pendingReducer = (state: StateType, action: ActionType) => {
+export const pendingReducer = (state: StateType, action: ActionType): void => {
   if (state.loading === Loading.idle) {
     state.loading = Loading.pending;
     state.currentRequestId = action.meta.requestId;
   }
 };
 
-export const fulfilledReducer = (state: StateType, action: ActionType) => {
+export const fulfilledReducer = (state: StateType, action: ActionType): void => {
   if (state.loading === Loading.pending && state.currentRequestId === action.meta.requestId) {
     state.loading = Loading.idle;
     state.entities = action.payload;
@@ -35,7 +35,7 @@ export const fulfilledReducer = (state: StateType, action: ActionType) => {
   }
 };
 
-export const rejectedReducer = (state: StateType, action: RejectedActionType) => {
+export const rejectedReducer = (state: StateType, action: RejectedActionType): void => {
   if (state.loading === Loading.pending && state.currentRequestId === action.meta.requestId) {
     state.loading = Loading.idle;
     state.error = action.error;
