3e9d8a23270ab10a77b90e1ecb33b8d3c2b09a2c
Justin Kim <justinkim943@gmail.com>
2020-08-09 00:01:15 -0700

    cleanup with constants

--- src/renderer/constants/store.ts
+++ src/renderer/constants/store.ts
@@ -2,18 +2,13 @@ const APP = 'app';
 export const BANKS = 'banks';
 export const VALIDATORS = 'validators';
 export const CONFIGS = 'configs';
-const NETWORK = 'network';
 
 // App
 export const ACTIVE_BANK = `${APP}/activeBank`;
 export const ACTIVE_PRIMARY_VALIDATOR = `${APP}/activePrimaryValidator`;
 
-// Configs
-export const VALIDATOR_CONFIGS = `${CONFIGS}/validatorConfigs`;
-
-// Network
-export const NETWORK_BANKS = `${NETWORK}/banks`;
-export const NETWORK_VALIDATORS = `${NETWORK}/validators`;
-
 // Banks
 export const BANK_CONFIGS = `${BANKS}/${CONFIGS}`;
+
+// Validators
+export const VALIDATOR_CONFIGS = `${VALIDATORS}/${CONFIGS}`;
