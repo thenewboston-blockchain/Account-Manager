1e9db16a820faf8711005aa1f336f48c6b155ba2
Bucky <buckyroberts@gmail.com>
2020-08-10 19:56:59 -0700

    Signature logic

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,39 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import axios from 'axios';
 
 import App from '@renderer/containers/App';
 import store from '@renderer/store';
 
-import {createAccount, signMessage} from './signing';
+import {generateBlock, keyPairFromSigningKeyHex} from './signing';
 import 'typeface-roboto';
 import 'normalize.css';
 import './styles/main.scss';
 
-const payload = JSON.stringify({
-  balance_key: '6cb8f4fe23c57a1c169e5a193c59ad9f21bbe5a54d69c44a4522f4644bf5b2a2',
-  txs: [
-    {
-      amount: 5.5,
-      recipient: '484b3176c63d5f37d808404af1a12c4b9649cd6f6769f35bdf5a816133623fbc',
-    },
-    {
-      amount: 1,
-      recipient: '5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8',
-    },
-    {
-      amount: 4,
-      recipient: 'ad1f8845c6a1abb6011a2a434a079a087c460657aad54329a84b406dce8bf314',
-    },
-  ],
-});
-console.error(payload);
+const txs = [
+  {
+    amount: 5.5,
+    recipient: '484b3176c63d5f37d808404af1a12c4b9649cd6f6769f35bdf5a816133623fbc',
+  },
+  {
+    amount: 1,
+    recipient: '5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8',
+  },
+  {
+    amount: 4,
+    recipient: 'ad1f8845c6a1abb6011a2a434a079a087c460657aad54329a84b406dce8bf314',
+  },
+];
 
-const {publicKeyHex, secretKey, secretKeyHex} = createAccount();
-const signature = signMessage(payload, secretKey);
-console.log(secretKeyHex);
-// console.log(publicKeyHex);
-console.log(signature);
+const fetchData = async (): Promise<void> => {
+  const signingKeyHex = 'e0ba29c1c493d01a5f665db55a4bd77caa140cf9722d0ed367ce4183230d2e02';
+  const balanceLock = 'ca6c8944fea472ad41523d77e413a2c464cbc0338be1fc3377e286c7d0c0e602';
+
+  const {publicKeyHex, secretKey} = keyPairFromSigningKeyHex(signingKeyHex);
+  const block = generateBlock(publicKeyHex, balanceLock, secretKey, txs);
+
+  const response = await axios.post('http://167.99.173.247/blocks', block, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  console.error(response);
+};
+
+fetchData();
 
 ReactDOM.render(
   <Provider store={store}>
--- src/renderer/signing.ts
+++ src/renderer/signing.ts
@@ -1,10 +1,61 @@
-import {sign} from 'tweetnacl';
+import {sign, SignKeyPair} from 'tweetnacl';
 
 export const createAccount = () => {
-  const {publicKey, secretKey} = sign.keyPair();
+  const keyPair = sign.keyPair();
+  return keyPairObject(keyPair);
+};
+
+// def generate_block(*, account_number, balance_lock, signing_key, transactions):
+// """
+// Generate block
+// """
+//
+// message = {
+//   'balance_key': balance_lock,
+//   'txs': sorted(transactions, key=itemgetter('recipient'))
+// }
+//
+// signature = generate_signature(
+//   message=sort_and_encode(message),
+//   signing_key=signing_key
+// )
+//
+// block = {
+//   'account_number': encode_verify_key(verify_key=account_number),
+//   'message': message,
+//   'signature': signature
+// }
+//
+// return block
+
+export const generateBlock = (
+  accountNumber: string,
+  balanceLock: string,
+  signingKey: Uint8Array,
+  transactions: any[],
+) => {
+  const message = {
+    balance_key: balanceLock,
+    txs: transactions,
+  };
+  const strMessage: string = JSON.stringify(message);
+  const block = {
+    account_number: accountNumber,
+    message,
+    signature: signMessage(strMessage, signingKey),
+  };
+  return JSON.stringify(block);
+};
+
+export const keyPairFromSigningKeyHex = (signingKeyHex: string) => {
+  const keyPair = sign.keyPair.fromSeed(Buffer.from(signingKeyHex, 'hex'));
+  return keyPairObject(keyPair);
+};
+
+const keyPairObject = (keyPair: SignKeyPair) => {
+  const {publicKey, secretKey} = keyPair;
   const publicKeyHex = Buffer.from(publicKey).toString('hex');
   const secretKeyHex = Buffer.from(secretKey).toString('hex');
-
   return {
     publicKey,
     publicKeyHex,
