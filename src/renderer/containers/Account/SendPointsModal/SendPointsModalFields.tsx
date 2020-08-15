5793fac4a0356f7e67c6510756e84cc987f53877
Bucky <buckyroberts@gmail.com>
2020-08-15 14:08:00 -0700

    Calculate correct amounts

--- src/renderer/containers/Account/SendPointsModal/SendPointsModalFields.tsx
+++ src/renderer/containers/Account/SendPointsModal/SendPointsModalFields.tsx
@@ -1,7 +1,10 @@
 import React, {FC} from 'react';
+import {useSelector} from 'react-redux';
+
 import {FormInput, FormSelectDetailed} from '@renderer/components/FormComponents';
 import RequiredAsterisk from '@renderer/components/RequiredAsterisk';
 import {useFormContext} from '@renderer/hooks';
+import {getActiveBankConfig, getActivePrimaryValidatorConfig} from '@renderer/selectors';
 import {Dict, ManagedAccount} from '@renderer/types';
 
 export const INVALID_AMOUNT_ERROR = 'Invalid amount';
@@ -13,21 +16,29 @@ interface ComponentProps {
 
 const SendPointsModalFields: FC<ComponentProps> = ({managedAccounts}) => {
   const {errors, values} = useFormContext();
+  const activeBankConfig = useSelector(getActiveBankConfig)!;
+  const activePrimaryValidatorConfig = useSelector(getActivePrimaryValidatorConfig)!;
   const invalidAmountError = errors.points === INVALID_AMOUNT_ERROR;
   const matchError = errors.fromAccountNumber === MATCH_ERROR || errors.toAccountNumber === MATCH_ERROR;
 
   const getFromOptions = () => {
-    return Object.values(managedAccounts).map(({account_number, nickname}) => ({
-      label: nickname,
-      value: account_number,
-    }));
+    const {toAccountNumber} = values;
+    return Object.values(managedAccounts)
+      .filter(({account_number}) => account_number !== toAccountNumber)
+      .map(({account_number, nickname}) => ({
+        label: nickname,
+        value: account_number,
+      }));
   };
 
   const getToOptions = () => {
-    return Object.values(managedAccounts).map(({account_number, nickname}) => ({
-      label: nickname,
-      value: account_number,
-    }));
+    const {fromAccountNumber} = values;
+    return Object.values(managedAccounts)
+      .filter(({account_number}) => account_number !== fromAccountNumber)
+      .map(({account_number, nickname}) => ({
+        label: nickname,
+        value: account_number,
+      }));
   };
 
   const renderFromAccountBalance = (): string => {
@@ -35,6 +46,15 @@ const SendPointsModalFields: FC<ComponentProps> = ({managedAccounts}) => {
     if (!fromAccountNumber) return '-';
     const {balance} = managedAccounts[fromAccountNumber];
     return balance || '0.00';
+  };
+
+  const renderTotal = (): number | string => {
+    const {points} = values;
+    if (!points) return '-';
+    const floatBalance = parseFloat(points);
+    const floatBankTxFee = parseFloat(activeBankConfig.default_transaction_fee);
+    const floatValidatorTxFee = parseFloat(activePrimaryValidatorConfig.default_transaction_fee);
+    return floatBalance + floatBankTxFee + floatValidatorTxFee;
   };
 
   return (
@@ -82,16 +102,16 @@ const SendPointsModalFields: FC<ComponentProps> = ({managedAccounts}) => {
           </tr>
           <tr>
             <td>Bank Registration Fee</td>
-            <td>0.01</td>
+            <td>{activeBankConfig.default_transaction_fee}</td>
           </tr>
           <tr>
             <td>Validator Tx Fee</td>
-            <td>0.02</td>
+            <td>{activePrimaryValidatorConfig.default_transaction_fee}</td>
           </tr>
           <tr className="SendPointsModal__total-tr">
             <td>Total</td>
             <td>
-              <b>0.00</b>
+              <b>{renderTotal()}</b>
             </td>
           </tr>
         </tbody>
