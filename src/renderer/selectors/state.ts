0d964790bcc3a264fa6389573d6d198135bcbe2f
Justin Kim <justinkim943@gmail.com>
2020-08-18 21:02:49 -0700

    Justin validator table links (#135)
    * calculateTotalCost params change
    
    * selectors refactor

---
+++ src/renderer/selectors/state.ts
@@ -0,0 +1,21 @@
+import {RootState} from '@renderer/types';
+
+export const getActiveBank = (state: RootState) => state.app.activeBank;
+export const getActivePrimaryValidator = (state: RootState) => state.app.activePrimaryValidator;
+export const getManagedAccounts = (state: RootState) => state.app.managedAccounts;
+export const getManagedValidators = (state: RootState) => state.app.managedValidators;
+
+export const getBankAccounts = (state: RootState) => state.banks.accounts;
+export const getBankBanks = (state: RootState) => state.banks.banks;
+export const getBankBankTransactions = (state: RootState) => state.banks.bank_transactions;
+export const getBankBlocks = (state: RootState) => state.banks.blocks;
+export const getBankConfigs = (state: RootState) => state.banks.configs;
+export const getBankConfirmationBlocks = (state: RootState) => state.banks.confirmation_blocks;
+export const getBankInvalidBlocks = (state: RootState) => state.banks.invalid_blocks;
+export const getBankValidatorConfirmationServices = (state: RootState) => state.banks.validator_confirmation_services;
+export const getBankValidators = (state: RootState) => state.banks.validators;
+
+export const getValidatorAccounts = (state: RootState) => state.validators.accounts;
+export const getValidatorBanks = (state: RootState) => state.validators.banks;
+export const getValidatorConfigs = (state: RootState) => state.validators.configs;
+export const getValidatorValidators = (state: RootState) => state.validators.validators;
