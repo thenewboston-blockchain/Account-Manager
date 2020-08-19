8122fcfb6a5c75649a707f788b3ffe0061a0263f
Justin Kim <justinkim943@gmail.com>
2020-08-18 19:23:09 -0700

    calculateTotalCost params change

--- src/renderer/utils/transactions.ts
+++ src/renderer/utils/transactions.ts
@@ -1,10 +1,4 @@
-interface TxAmounts {
-  bankTxFee: string;
-  points: number;
-  validatorTxFee: string;
-}
-
-export const calculateTotalCost = ({bankTxFee, points, validatorTxFee}: TxAmounts): number => {
+export const calculateTotalCost = (bankTxFee: string, points: number, validatorTxFee: string): number => {
   const floatBankTxFee = parseFloat(bankTxFee);
   const floatValidatorTxFee = parseFloat(validatorTxFee);
   return floatBankTxFee + floatValidatorTxFee + points;
