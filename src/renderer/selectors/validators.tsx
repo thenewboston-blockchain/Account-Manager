4b071384c7720818a826c507e2060c16d894e014
Justin Kim <justinkim943@gmail.com>
2020-08-20 19:44:46 -0700

    use inline function

--- src/renderer/selectors/validators.tsx
+++ src/renderer/selectors/validators.tsx
@@ -4,12 +4,12 @@ import memoize from 'lodash/memoize';
 import {formatAddressFromNode} from '@renderer/utils/address';
 import {getActivePrimaryValidator, getManagedValidators} from './state';
 
-const getIsActivePrimaryValidatorFn = (address: string) =>
+export const getIsActivePrimaryValidator = memoize((address: string) =>
   createSelector([getActivePrimaryValidator, () => address], (activePrimaryValidator) =>
     activePrimaryValidator ? formatAddressFromNode(activePrimaryValidator) === address : false,
-  );
-export const getIsActivePrimaryValidator = memoize(getIsActivePrimaryValidatorFn);
+  ),
+);
 
-const getIsManagedValidatorFn = (address: string) =>
-  createSelector([getManagedValidators, () => address], (managedValidators) => !!managedValidators[address]);
-export const getIsManagedValidator = memoize(getIsManagedValidatorFn);
+export const getIsManagedValidator = memoize((address: string) =>
+  createSelector([getManagedValidators, () => address], (managedValidators) => !!managedValidators[address]),
+);
