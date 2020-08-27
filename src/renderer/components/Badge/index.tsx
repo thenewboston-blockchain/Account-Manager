5a42bf0f81d992901b69d2481a406e14568729f0
Justin Kim <justinkim943@gmail.com>
2020-08-26 18:32:44 -0700

    cleanup

--- src/renderer/components/Badge/index.tsx
+++ src/renderer/components/Badge/index.tsx
@@ -1,8 +1,8 @@
 import React, {FC} from 'react';
 import clsx from 'clsx';
 
-import './Badge.scss';
 import {getCustomClassNames} from '@renderer/utils/components';
+import './Badge.scss';
 
 interface ComponentProps {
   className?: string;
