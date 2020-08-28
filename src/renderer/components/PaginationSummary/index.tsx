03106f7bdaf7599335da332677edaa65aa0444ef
Bucky <buckyroberts@gmail.com>
2020-08-27 21:27:46 -0700

    Temp fix

--- src/renderer/components/PaginationSummary/index.tsx
+++ src/renderer/components/PaginationSummary/index.tsx
@@ -14,7 +14,7 @@ interface ComponentProps {
 const PaginationSummary: FC<ComponentProps> = ({className, count, currentPage}) => {
   const firstRow = Math.min((currentPage - 1) * PAGINATED_RESULTS_LIMIT + 1, count);
   const lastRow = Math.min(currentPage * PAGINATED_RESULTS_LIMIT, count);
-  const summary = `${firstRow}–${lastRow} of ${count}`;
+  const summary = `${firstRow}-${lastRow} of ${count}`;
 
   return <div className={clsx('PaginationSummary', className)}>{summary}</div>;
 };
