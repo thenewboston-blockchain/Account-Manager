0d1662009279ef9fddbf62d40f0126e828853bd3
Justin Kim <justinkim943@gmail.com>
2020-09-01 21:06:30 -0700

    removed reload

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,18 +1,105 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console  */
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-import {app, BrowserWindow} from 'electron';
+import {app, BrowserWindow, Menu} from 'electron';
 import contextMenu from 'electron-context-menu';
 import installExtension, {REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} from 'electron-devtools-installer';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const electronSquirrelStartup = require('electron-squirrel-startup');
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
+
+const isMac = process.platform === 'darwin';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (electronSquirrelStartup) {
   app.quit();
 }
+
+const template = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {role: 'about'},
+            {type: 'separator'},
+            {role: 'services'},
+            {type: 'separator'},
+            {role: 'hide'},
+            {role: 'hideothers'},
+            {role: 'unhide'},
+            {type: 'separator'},
+            {role: 'quit'},
+          ],
+        },
+      ]
+    : []),
+  {
+    label: 'File',
+    submenu: [isMac ? {role: 'close'} : {role: 'quit'}],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      ...(isMac
+        ? [
+            {role: 'pasteAndMatchStyle'},
+            {role: 'delete'},
+            {role: 'selectAll'},
+            {type: 'separator'},
+            {
+              label: 'Speech',
+              submenu: [{role: 'startspeaking'}, {role: 'stopspeaking'}],
+            },
+          ]
+        : [{role: 'delete'}, {type: 'separator'}, {role: 'selectAll'}]),
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'},
+    ],
+  },
+  {
+    label: 'Window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'zoom'},
+      ...(isMac ? [{type: 'separator'}, {role: 'front'}, {type: 'separator'}, {role: 'window'}] : [{role: 'close'}]),
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        click: async () => {
+          const {shell} = require('electron');
+          await shell.openExternal('https://electronjs.org');
+        },
+        label: 'Learn More',
+      },
+    ],
+  },
+] as Electron.MenuItemConstructorOptions[];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 contextMenu();
 
@@ -27,6 +114,8 @@ const createWindow = (): void => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 };
 
+app.setName('TheNewBoston');
+
 app.whenReady().then(() => {
   installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
     .then((name) => console.log(`Added Extension: ${name}`))
@@ -38,7 +127,7 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (!isMac) {
     app.quit();
   }
 });
