77f1afc4f2e1e706699158565c7cf3539a1cf643
Bucky <buckyroberts@gmail.com>
2020-09-05 12:30:29 -0700

    More work on notifications

--- src/renderer/containers/TopNav/TopNavNotifications/index.tsx
+++ src/renderer/containers/TopNav/TopNavNotifications/index.tsx
@@ -1,10 +1,12 @@
 import React, {FC, ReactNode, useEffect, useRef, useState} from 'react';
 import {createPortal} from 'react-dom';
+import {useSelector} from 'react-redux';
 import {NavLink, useLocation} from 'react-router-dom';
 import clsx from 'clsx';
 
 import Icon, {IconType} from '@renderer/components/Icon';
 import {useBooleanState} from '@renderer/hooks';
+import {getManagedAccounts, getManagedFriends} from '@renderer/selectors';
 import {displayErrorToast} from '@renderer/utils/toast';
 
 import TopNavNotificationsMenu from './TopNavNotificationsMenu';
@@ -14,9 +16,11 @@ const dropdownRoot = document.getElementById('dropdown-root')!;
 
 const TopNavNotifications: FC = () => {
   const {pathname} = useLocation();
-  const iconRef = useRef<HTMLDivElement>(null);
   const [open, toggleOpen, , closeMenu] = useBooleanState(false);
   const [notifications, setNotifications] = useState<ReactNode[]>([]);
+  const iconRef = useRef<HTMLDivElement>(null);
+  const managedAccounts = useSelector(getManagedAccounts);
+  const managedFriends = useSelector(getManagedFriends);
 
   useEffect(() => {
     const accountNumber = '4ed6c42c98a9f9b521f434df41e7de87a1543940121c895f3fb383bb8585d3ec';
@@ -28,13 +32,28 @@ const TopNavNotifications: FC = () => {
     closeMenu();
   }, [pathname, closeMenu]);
 
-  const getNotifications = (notificationType: string, payload: any) => {
-    console.log(notificationType, payload);
+  const getAccountNickname = (accountNumber: string): string => {
+    const managedAccount = managedAccounts[accountNumber];
+
+    if (managedAccount) {
+      return managedAccount.nickname || managedAccount.account_number;
+    }
+
+    const managedFriend = managedFriends[accountNumber];
+
+    if (managedFriend) {
+      return managedFriend.nickname || managedFriend.account_number;
+    }
+
+    return accountNumber;
+  };
+
+  const getNotifications = (notificationType: string, payload: any): ReactNode[] => {
     switch (notificationType) {
       case 'CONFIRMATION_BLOCK_NOTIFICATION':
         return renderConfirmationReceivedNotification(payload);
       default:
-        return null;
+        return [];
     }
   };
 
@@ -48,7 +67,7 @@ const TopNavNotifications: FC = () => {
     }
   };
 
-  const renderConfirmationReceivedNotification = (payload: any): ReactNode => {
+  const renderConfirmationReceivedNotification = (payload: any): ReactNode[] => {
     const {
       message: {
         block: {
@@ -58,8 +77,6 @@ const TopNavNotifications: FC = () => {
       },
     } = payload;
 
-    console.log(txs);
-
     return txs.map(({amount, recipient: recipientAccountNumber}: any) => (
       <div className="TopNavNotificationsMenu__notification" key={recipientAccountNumber}>
         <Icon className="TopNavNotificationsMenu__Icon" icon={IconType.checkboxBlankCircle} size={8} />
@@ -67,16 +84,16 @@ const TopNavNotifications: FC = () => {
           <div className="TopNavNotificationsMenu__description">
             <div>
               <NavLink className="TopNavNotificationsMenu__NavLink" to={`/account/${senderAccountNumber}/overview`}>
-                {senderAccountNumber}
+                {getAccountNickname(senderAccountNumber)}
               </NavLink>{' '}
               paid you{' '}
               <NavLink className="TopNavNotificationsMenu__NavLink" to={`/account/${recipientAccountNumber}/overview`}>
-                (Personal)
+                ({getAccountNickname(recipientAccountNumber)})
               </NavLink>
             </div>
             <div className="TopNavNotificationsMenu__time">1h ago</div>
           </div>
-          <div className="TopNavNotificationsMenu__amount">+ amount</div>
+          <div className="TopNavNotificationsMenu__amount">+ {amount}</div>
         </div>
       </div>
     ));
