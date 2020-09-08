20c0910b586cfc584f08faee93bf79ddc50e2cd0
Bucky <buckyroberts@gmail.com>
2020-09-07 19:16:58 -0700

    Detailed select until we can figure out something better (may have null account numbers)

--- src/renderer/containers/PurchaseConfirmationServicesModal/index.tsx
+++ src/renderer/containers/PurchaseConfirmationServicesModal/index.tsx
@@ -1,7 +1,10 @@
-import React, {FC} from 'react';
+import React, {FC, useMemo, useState} from 'react';
+import {useSelector} from 'react-redux';
 
-import {FormInput} from '@renderer/components/FormComponents';
+import {FormSelectDetailed} from '@renderer/components/FormComponents';
 import Modal from '@renderer/components/Modal';
+import {getManagedBanks} from '@renderer/selectors';
+import {InputOption} from '@renderer/types';
 
 import './PurchaseConfirmationServicesModal.scss';
 
@@ -10,6 +13,18 @@ interface ComponentProps {
 }
 
 const PurchaseConfirmationServicesModal: FC<ComponentProps> = ({close}) => {
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const managedBanks = useSelector(getManagedBanks);
+
+  const getFromOptions = useMemo<InputOption[]>(
+    () =>
+      Object.values(managedBanks).map(({ip_address, nickname}) => ({
+        label: nickname,
+        value: ip_address,
+      })),
+    [managedBanks],
+  );
+
   return (
     <Modal
       className="PurchaseConfirmationServicesModal"
@@ -18,10 +33,18 @@ const PurchaseConfirmationServicesModal: FC<ComponentProps> = ({close}) => {
       initialValues={{}}
       onSubmit={() => {}}
       submitButton="Purchase"
-      submitting={false}
+      submitting={submitting}
       validationSchema={{}}
     >
-      <FormInput focused label="Trust" name="trust" required type="number" />
+      <FormSelectDetailed
+        className="SendPointsModal__select"
+        disabled={submitting}
+        focused
+        required
+        label="From: Managed Bank"
+        options={getFromOptions}
+        name="managedBank"
+      />
     </Modal>
   );
 };
