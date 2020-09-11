344687ab06ce6844edcf64bca64c10e9ffd37b6c
tpikachu <lee.michael7@yahoo.com>
2020-09-11 09:38:10 -0400

    1.0.0-beta.4

--- app-information.js
+++ app-information.js
@@ -3,7 +3,8 @@ const packageJson = require("./package.json");
 const appName = packageJson.productName;
 const version = packageJson.version;
 const mode = version.includes("beta") ? "beta" : version.includes("alpha") ? "alpha" : "stable";
-const appId = "com." + (mode === "stable" ? "" : mode + ".") + "thenewboston.account.manager.app";
+//const appId = "com." + (mode === "stable" ? "" : mode + ".") + "thenewboston.account.manager.app";
+const appId = "com.thenewboston.account.manager.app";
 const subVersion = version.slice(version.indexOf(mode));
 
 module.exports = {
