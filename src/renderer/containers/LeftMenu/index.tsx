2f8379d1203082567a353bf32037d28142d7fb63
Bucky <buckyroberts@gmail.com>
2020-09-11 14:36:31 -0700

    Tweaks

--- src/renderer/containers/LeftMenu/index.tsx
+++ src/renderer/containers/LeftMenu/index.tsx
@@ -121,13 +121,13 @@ const LeftMenu: FC = () => {
   return (
     <div className="LeftMenu">
       <div className="points">
-        <div className="points__title">Points</div>
+        <div className="points__title">Balance</div>
         <div className="points__amount">{pointBalance.toLocaleString()}</div>
       </div>
+      <LeftSubmenu menuItems={validatorMenuItems} rightOnClick={toggleAddValidatorModal} title="Validators" />
+      <LeftSubmenu menuItems={bankMenuItems} rightOnClick={toggleAddBankModal} title="Banks" />
       <LeftSubmenu menuItems={accountItems} rightOnClick={toggleCreateAccountModal} title="Accounts" />
       <LeftSubmenu menuItems={friendMenuItems} rightOnClick={toggleAddFriendModal} title="Friends" />
-      <LeftSubmenu menuItems={bankMenuItems} rightOnClick={toggleAddBankModal} title="Managed Banks" />
-      <LeftSubmenu menuItems={validatorMenuItems} rightOnClick={toggleAddValidatorModal} title="Managed Validators" />
       {addFriendModalIsOpen && <AddFriendModal close={toggleAddFriendModal} />}
       {addBankModalIsOpen && <AddBankModal close={toggleAddBankModal} />}
       {addValidatorModalIsOpen && <AddValidatorModal close={toggleAddValidatorModal} />}
