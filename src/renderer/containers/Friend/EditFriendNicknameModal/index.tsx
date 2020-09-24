c2e67993c42e686064d048e9d688f9e0e72c370e
Rostislavs Pavlovs <ros.pavlov@outlook.com>
2020-09-24 20:05:40 +0800

    added yup validation for friends names

--- src/renderer/containers/Friend/EditFriendNicknameModal/index.tsx
+++ src/renderer/containers/Friend/EditFriendNicknameModal/index.tsx
@@ -1,9 +1,11 @@
-import React, {FC} from 'react';
-import {useDispatch} from 'react-redux';
+import React, {FC, useMemo} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import Modal from '@renderer/components/Modal';
+import {getManagedFriends} from '@renderer/selectors';
 import {FormInput} from '@renderer/components/FormComponents';
 import {setManagedFriend} from '@renderer/store/app';
 import {AppDispatch, ManagedFriend} from '@renderer/types';
+import yup from '@renderer/utils/yup';
 
 interface ComponentProps {
   close(): void;
@@ -12,11 +14,20 @@ interface ComponentProps {
 
 const EditFriendNicknameModal: FC<ComponentProps> = ({close, managedFriend}) => {
   const dispatch = useDispatch<AppDispatch>();
+  const managedFriends = useSelector(getManagedFriends);
 
   const initialValues = {
     nickname: managedFriend.nickname,
   };
   type FormValues = typeof initialValues;
+
+  const managedFriendNicknames = useMemo(
+    () =>
+      Object.values(managedFriends)
+        .filter(({nickname}) => initialValues.nickname !== nickname)
+        .map(({nickname}) => nickname),
+    [managedFriends, initialValues],
+  );
 
   const handleSubmit = ({nickname}: FormValues): void => {
     dispatch(
@@ -28,6 +39,13 @@ const EditFriendNicknameModal: FC<ComponentProps> = ({close, managedFriend}) => 
     close();
   };
 
+  const validationSchema = useMemo(() => {
+    console.log(managedFriendNicknames);
+    return yup.object().shape({
+      nickname: yup.string().notOneOf(managedFriendNicknames, 'That nickname is already taken'),
+    });
+  }, [managedFriendNicknames]);
+
   return (
     <Modal
       cancelButton="Cancel"
@@ -36,6 +54,7 @@ const EditFriendNicknameModal: FC<ComponentProps> = ({close, managedFriend}) => 
       initialValues={initialValues}
       onSubmit={handleSubmit}
       submitButton="Save"
+      validationSchema={validationSchema}
     >
       <FormInput focused label="Friend Nickname" name="nickname" />
     </Modal>
