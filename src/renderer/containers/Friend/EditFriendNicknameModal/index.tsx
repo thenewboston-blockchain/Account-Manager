ded3ee49c141aa2c6283decba3f9afd9a56f6dbe
Ros Pavlov <avgust13@users.noreply.github.com>
2020-09-25 13:59:54 +0800

    Fixed bug: user should not get added with same nickname while editing nickname (#325)
    * added yup validation for friends names
    
    * Cleaning
    
    Co-authored-by: Rostislavs Pavlovs <ros.pavlov@outlook.com>
    Co-authored-by: Bucky <buckyroberts@gmail.com>

--- src/renderer/containers/Friend/EditFriendNicknameModal/index.tsx
+++ src/renderer/containers/Friend/EditFriendNicknameModal/index.tsx
@@ -1,9 +1,12 @@
-import React, {FC} from 'react';
-import {useDispatch} from 'react-redux';
+import React, {FC, useMemo} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+
 import Modal from '@renderer/components/Modal';
 import {FormInput} from '@renderer/components/FormComponents';
+import {getManagedFriends} from '@renderer/selectors';
 import {setManagedFriend} from '@renderer/store/app';
 import {AppDispatch, ManagedFriend} from '@renderer/types';
+import yup from '@renderer/utils/yup';
 
 interface ComponentProps {
   close(): void;
@@ -12,11 +15,20 @@ interface ComponentProps {
 
 const EditFriendNicknameModal: FC<ComponentProps> = ({close, managedFriend}) => {
   const dispatch = useDispatch<AppDispatch>();
+  const managedFriends = useSelector(getManagedFriends);
 
   const initialValues = {
     nickname: managedFriend.nickname,
   };
   type FormValues = typeof initialValues;
+
+  const managedFriendsNicknames = useMemo(
+    () =>
+      Object.values(managedFriends)
+        .filter(({nickname}) => initialValues.nickname !== nickname)
+        .map(({nickname}) => nickname),
+    [initialValues, managedFriends],
+  );
 
   const handleSubmit = ({nickname}: FormValues): void => {
     dispatch(
@@ -28,6 +40,12 @@ const EditFriendNicknameModal: FC<ComponentProps> = ({close, managedFriend}) => 
     close();
   };
 
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      nickname: yup.string().notOneOf(managedFriendsNicknames, 'That nickname is already taken'),
+    });
+  }, [managedFriendsNicknames]);
+
   return (
     <Modal
       cancelButton="Cancel"
@@ -36,6 +54,7 @@ const EditFriendNicknameModal: FC<ComponentProps> = ({close, managedFriend}) => 
       initialValues={initialValues}
       onSubmit={handleSubmit}
       submitButton="Save"
+      validationSchema={validationSchema}
     >
       <FormInput focused label="Friend Nickname" name="nickname" />
     </Modal>
