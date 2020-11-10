53501d878e06a90cdb30c890fc796adbdf6abf23
chad Vandermark <Chad.vdmk@gmail.com>
2020-11-10 16:21:55 -0500

    [Issue-438]  points/coins amount too large issue - added check to see if amount entered is over 999999999999999 - solves issue with amount being updated to 3 -solves potential issue with rounding up amount when over 999999999999999

--- src/renderer/containers/SendCoinsModal/index.tsx
+++ src/renderer/containers/SendCoinsModal/index.tsx
@@ -96,6 +96,7 @@ const SendCoinsModal: FC<ComponentProps> = ({close, initialRecipient, initialSen
         .number()
         .callbackWithRef(senderAccountNumberRef, checkCoinsWithBalance, INVALID_AMOUNT_ERROR)
         .moreThan(0, 'Coins must be greater than 0')
+        .max(999999999999999, 'Coins amount too large')
         .integer('Coins cannot be a decimal')
         .required('Coins is a required field'),
       recipientAccountNumber: yup
