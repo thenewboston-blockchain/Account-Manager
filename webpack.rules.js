c226837279b3d81b92f29008a67a9df84e94557b
Justin Kim <justinkim943@gmail.com>
2021-02-02 21:52:05 -0800

    sass absolute import

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 const sass = require('sass');
 
 const rules = [
@@ -41,7 +42,7 @@ const rules = [
       {
         loader: 'sass-loader',
         options: {
-          sassOptions: {implementation: sass, includePaths: ['src/renderer'], indentWidth: 2},
+          sassOptions: {implementation: sass, includePaths: [path.join(__dirname, 'src/renderer')], indentWidth: 2},
         },
       },
     ],
