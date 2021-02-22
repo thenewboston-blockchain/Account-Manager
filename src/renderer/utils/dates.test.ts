20505b16a61a64244c9a8dad9dfe599227002fbf
jamessspanggg <jamespangmunwai@gmail.com>
2021-02-22 21:28:14 +0800

    Fix formatDate test case

--- src/renderer/utils/dates.test.ts
+++ src/renderer/utils/dates.test.ts
@@ -2,7 +2,7 @@ import {formatDate} from './dates';
 
 describe('formatDate to return the following:', () => {
   test('correctly formatted date', () => {
-    expect(formatDate('2020-08-28T16:26:19.902626Z')).toEqual('2020-08-28 09:26:19 AM');
+    expect(formatDate('2020-08-28T16:26:19.902626Z')).toEqual('2020-08-28 04:26:19 PM');
   });
   test('return empty string for empty date', () => {
     expect(formatDate('')).toEqual('');
