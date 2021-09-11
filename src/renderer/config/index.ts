7e586575a37c34920622b7aa2cccb13b5a319c9d
wakawaka <89671440+wakawakathedev@users.noreply.github.com>
2021-09-12 00:25:30 +0800

    ADD: fix last semicolon;

--- src/renderer/config/index.ts
+++ src/renderer/config/index.ts
@@ -6,4 +6,4 @@ export const defaultPaginatedQueryParam = {
 
 export const AXIOS_TIMEOUT_MS = 10_000;
 
-export const DEFAULT_BANK_IP_ADDRESS = '54.183.16.194'
+export const DEFAULT_BANK_IP_ADDRESS = '54.183.16.194';
