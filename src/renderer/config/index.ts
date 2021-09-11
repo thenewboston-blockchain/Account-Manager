d36dc53da143e9475c345a8a6efb4850538fe391
angle943 <justinkim943@gmail.com>
2021-09-11 11:30:10 -0500

    make default_bank the constant

--- src/renderer/config/index.ts
+++ src/renderer/config/index.ts
@@ -1,3 +1,5 @@
+import {ProtocolType} from '@renderer/types';
+
 export const PAGINATED_RESULTS_LIMIT = 30;
 
 export const defaultPaginatedQueryParam = {
@@ -6,4 +8,8 @@ export const defaultPaginatedQueryParam = {
 
 export const AXIOS_TIMEOUT_MS = 10_000;
 
-export const DEFAULT_BANK_IP_ADDRESS = '54.183.16.194';
+export const DEFAULT_BANK = {
+  ip_address: '54.183.16.194',
+  port: 80,
+  protocol: 'http' as ProtocolType,
+};
